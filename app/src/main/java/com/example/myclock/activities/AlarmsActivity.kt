b97181d61d62f2e54204fe5874675a2ed6034972
Ralph Degoma <personal.ralphdegoma@gmail.com>
2021-05-23 17:07:25 +0800

    Initial

---
+++ app/src/main/java/com/example/myclock/activities/AlarmsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myclock.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myclock.R
+
+class AlarmsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarms)
+    }
+}
